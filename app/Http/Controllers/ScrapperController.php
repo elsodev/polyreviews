f90b6336d19ae03ea1c2eab521e76f92cc43ef1f
Elson Tan <elsodev.et@gmail.com>
2016-10-16 20:47:30 +0800

    UPDATE FB scrapper

--- app/Http/Controllers/ScrapperController.php
+++ app/Http/Controllers/ScrapperController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\App;
 use Serps\SearchEngine\Google\GoogleClient;
 use Serps\HttpClient\CurlClient;
@@ -15,6 +14,12 @@ class ScrapperController extends Controller
     // for scrapper to identify itself as a browser
     private $default_user_agent = "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36";
 
+    private $fbToken;
+
+    public function __construct()
+    {
+        $this->fbToken = config('laravel-facebook-sdk.facebook_config.app_id').'|'.config('laravel-facebook-sdk.facebook_config.app_secret');
+    }
 
     public function scrapGoogle($keyword)
     {
@@ -41,10 +46,9 @@ class ScrapperController extends Controller
     {
 
         $fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
-        $token = config('laravel-facebook-sdk.facebook_config.app_id').'|'.config('laravel-facebook-sdk.facebook_config.app_secret');
 
         try {
-            $responses = $fb->get('/search?q='.$keyword.'&type=place&access_token='.$token)->getDecodedBody();
+            $responses = $fb->get('/search?q='.$keyword.'&type=place&access_token='.$this->fbToken)->getDecodedBody();
             return $responses;
 
         } catch(FacebookSDKException $e) {
@@ -52,6 +56,21 @@ class ScrapperController extends Controller
         }
         
         return null;
+    }
+
+    public function getFacebookData($places)
+    {
+        $fb = App::make('SammyK\LaravelFacebookSdk\LaravelFacebookSdk');
+
+        // obtain all info about the location
+        foreach($places as $place) {
+            try{
+                $response = $fb->get('/'.$place['id'].'?access_token='.$this->fbToken.'&fields=id,name,location')->getDecodedBody();
+            } catch(FacebookSDKException $e) {
+                dd($e->getMessage());
+            }
+
+        }
     }
 
 }
